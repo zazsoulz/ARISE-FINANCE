0fff63e3785ba0b0f328e68fa45c36598a40c7e8
zazsoulz <zazsoulz.work@gmail.com>
2026-08-20 01:55:36 +0300

    Keep public A1-V3 month summary backward compatible

--- financial-bootstrap.js
+++ financial-bootstrap.js
@@ -1,6 +1,17 @@
 (function(){
   "use strict";
 
+  const v3=globalThis.ARISE_V3;
+  if(v3&&typeof v3.groupMonth==="function"&&!v3.__publicGroupWrapped){
+    const internalGroupMonth=v3.groupMonth;
+    v3.groupMonth=function(profile,month){
+      const data=internalGroupMonth(profile,month);
+      const {uncontrolled,...publicData}=data;
+      return publicData;
+    };
+    v3.__publicGroupWrapped=true;
+  }
+
   try{
     if(state.account.registered){
       render();
